8b067d940978923889b5c1b0fca189fc04cd9d39
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2025-03-12 21:41:58 +0900

    [Silver I] Title: IOIOI, Time: 256 ms, Memory: 20608 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -5,20 +5,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // PN에서 OI의 개수
+        int M = Integer.parseInt(br.readLine()); // 문자열 S의 길이
         String S = br.readLine();
 
-        String pn = "I";
+        int count = 0; // PN의 개수를 셀 변수
+        int pattern = 0; // 'OI' 패턴의 연속 횟수
 
-        pn += "OI".repeat(N);
+        for (int i = 0; i < M - 1; i++) {
+            // 'I'로 시작하는 경우만 패턴 검사
+            if (S.charAt(i) == 'I') {
+                // 연속된 'OI'의 개수를 확인
+                while (i + 1 < M && S.charAt(i + 1) == 'O' && i + 2 < M && S.charAt(i + 2) == 'I') {
+                    pattern++; // 'OI'가 나올 때마다 증가
+                    i += 2; // 'OI' 검사했으므로 두 칸 이동
 
-        int count = 0;
-        int index = 0;
-
-        while ((index = S.indexOf(pn, index)) != -1) {
-            count++;
-            index++;
+                    // N번의 'OI'가 연속되면 PN 패턴 완성
+                    if (pattern == N) {
+                        count++; 
+                        pattern--; // 중복되는 부분을 유지하기 위해 감소
+                    }
+                }
+                // 'OI' 패턴이 끊기면 초기화
+                pattern = 0;
+            }
         }
 
         bw.write(count + "\n");
