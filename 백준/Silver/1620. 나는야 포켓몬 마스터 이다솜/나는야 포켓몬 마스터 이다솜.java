fa98d25635d359a4f906f7b44851a52cf723e4fb
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2025-04-02 22:51:25 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 696 ms, Memory: 105824 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<Integer, String> num = new HashMap<>();
+        Map<String, Integer> name = new HashMap<>();
+
+        for (int i = 1; i < N + 1; i++) {
+            String input = br.readLine();
+            name.put(input, i);
+            num.put(i, input);
+        }
+
+        for (int i = 0; i < M; i++) {
+            String input = br.readLine();
+
+            try {
+                int idx = Integer.parseInt(input);
+                sb.append(num.get(idx)).append("\n");
+            } catch (NumberFormatException e) {
+                // TODO: 블로그 글 : +1 한 거 적기
+                sb.append(name.get(input)).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
