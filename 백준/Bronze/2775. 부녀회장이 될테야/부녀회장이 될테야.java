db633e127733463f8d56c0fc7e72eb8087dc9a50
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2025-04-16 23:03:58 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 96 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,46 +1,40 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
-    private static final int[][] apart = new int[15][15];
+    private static int[][] apart;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        apart = new int[15][15];
         makeApart();
 
         for (int i = 0; i < T; i++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
-
-            int count = apart[k][n];
-            bw.write(count + "\n");
+            sb.append(apart[k][n]).append("\n");
         }
-        br.close();
-        bw.flush();
-        bw.close();
 
+        System.out.println(sb);
+        br.close();
     }
 
 
     private static void makeApart() {
         for (int i = 0; i < 15; i++) {
             for (int j = 1; j < 15; j++) {
-                if (i == 0) {
-                    apart[i][j] = j;
+                if (i == 0){
+                    apart[0][j] = j;
                     continue;
                 }
 
                 apart[i][j] = apart[i][j - 1] + apart[i - 1][j];
             }
         }
+
+
     }
-
-
 }
