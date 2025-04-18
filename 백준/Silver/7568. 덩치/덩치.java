ddddddf3e1538e1d87eedf0c833cd377993cbef9
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2025-04-18 22:45:35 +0900

    [Silver V] Title: 덩치, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,39 +1,55 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[][] people = new int[N][2];
+        Person[] people = new Person[N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            people[i][0] = x;
-            people[i][1] = y;
+            Person person = new Person(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            people[i] = person;
         }
 
-        for (int[] person1 : people) {
-            int count = 1;
-            for (int[] person2 : people) {
-                if ((person1[0] < person2[0]) && (person1[1] < person2[1])) {
-                    count++;
+        StringBuilder sb = new StringBuilder();
+
+        for (Person person1 : people) {
+            int rank = 1;
+            for (Person person2 : people) {
+                if (person2.compareTo(person1) > 0) {
+                    rank++;
                 }
             }
-            bw.write(count + " ");
+            sb.append(rank).append(" ");
         }
 
-        bw.write("\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
+
+
+    }
+
+
+    private static class Person implements Comparable<Person> {
+
+        private final int weight;
+        private final int height;
+
+        private Person(int weight, int height) {
+            this.weight = weight;
+            this.height = height;
+        }
+
+        @Override
+        public int compareTo(Person o) {
+            if (this.weight > o.weight && this.height > o.height) {
+                return 1;
+            }
+
+            return 0;
+        }
     }
 }
