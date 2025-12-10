9e0663eacd6089abc2461035271f95617dfa5c2b
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2025-12-10 21:25:15 +0900

    [Bronze V] Title: A/B, Time: 112 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -2,17 +2,13 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-
-        double margin = Math.pow(10, 9);
-        double result = a * margin / b / margin;
         
-        System.out.println(result);
-    }
+        Double a = Double.parseDouble(st.nextToken());
+        Double b = Double.parseDouble(st.nextToken());
 
+        System.out.print(a / b);
+    }
 }
