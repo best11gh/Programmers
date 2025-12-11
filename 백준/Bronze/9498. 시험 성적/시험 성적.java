2accd3e7b2e22cc34e33f57ca83cba0ccef323e8
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2025-12-11 15:17:27 +0900

    [Bronze V] Title: 시험 성적, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int score = Integer.parseInt(br.readLine());
+
+        System.out.println(score >= 90 ? "A" : score >= 80 ? "B" : score >= 70 ? "C" : score >= 60 ? "D" : "F");
+    }
+}
