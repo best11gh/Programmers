43e548c27cc89039826beb4e7b14b3dadf745184
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2025-12-15 17:21:02 +0900

    [Bronze IV] Title: 영수증, Time: 108 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int total = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            total += a * b;
+        }
+
+        System.out.println(x==total ? "Yes" : "No");
+    }
+}
