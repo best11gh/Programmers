a2caf8a753332b74d72044fbde95f3cd64af7c94
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2025-12-17 10:50:23 +0900

    [Bronze V] Title: A+B - 5, Time: 112 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                br.close();
+                bw.flush();
+                bw.close();
+                return;
+            }
+
+            bw.write(String.valueOf(a + b));
+            bw.newLine();
+        }
+    }
+}
