6fa95cd3a985490129ddca7dfebcfae374c87a7a
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2025-12-18 17:17:19 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int idx = 0;
+        int max = 0;
+
+        for (int i = 1; i < 10; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num > max) {
+                max = num;
+                idx = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(idx);
+    }
+}
