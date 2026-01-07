51e3ab2bfd2c5dae0771b78e25155eceacd18aa5
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-01-07 09:18:41 +0900

    [Bronze II] Title: 소수, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int min = 0;
+
+        for (int num = M; num <= N; num++) {
+            if (isPrime(num)) {
+                sum += num;
+
+                if (min == 0) {
+                    min = num;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+
+    private static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
