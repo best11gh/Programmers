b5ce7955f3dea40c937a1cfb27b7c9f9f6c158d4
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-01-12 00:52:08 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,33 +1,23 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        br.close();
 
-        int count = N / 5 + 1;
-        int result = Integer.MAX_VALUE;
+        int max = N / 5;
+        int count = -1;
 
-        for (int i = count - 1; i >= 0; i--) {
-            int remain = N - (5 * i);
+        for (int i = max; i >= 0; i--) {
+            int remain = N - i * 5;
+
             if (remain % 3 == 0) {
-                result = Math.min(result, remain / 3 + i);
+                count = i + (remain / 3);
+                break;
             }
         }
 
-        if (result == Integer.MAX_VALUE) {
-            result = -1;
-        }
-
-        bw.write(result + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(count);
     }
 }
