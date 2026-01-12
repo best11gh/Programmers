a0f3ff4c145f1f6a50e070604cac0eb53c78acea
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-01-12 11:33:06 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,72 +1,50 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
+    private static boolean[][] arr; // true: B, false: W
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        String[] board = new String[N];
+        arr = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
-            String row = br.readLine();
-            board[i] = row;
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = line.charAt(j) == 'B';
+            }
         }
 
         int result = 64;
+
         for (int i = 0; i < N - 7; i++) {
             for (int j = 0; j < M - 7; j++) {
-                String[] subBoard = Arrays.copyOfRange(board, i, i + 8);
-                result = Math.min(getChangeCount(subBoard, j), result);
+                result = Math.min(result, getRepaintCount(i, j));
             }
         }
 
-        bw.write(result + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(result);
     }
 
-
-    private static int getChangeCount(String[] board, int colCount) {
-        String case1;
-        String case2;
+    private static int getRepaintCount(int x, int y) {
+        boolean firstColor = arr[x][y];
         int count = 0;
 
-        char firstColor = board[0].charAt(colCount);
-        if (firstColor == 'W') {
-            case1 = "WBWBWBWB";
-            case2 = "BWBWBWBW";
-        } else {
-            case1 = "BWBWBWBW";
-            case2 = "WBWBWBWB";
-        }
-
         for (int i = 0; i < 8; i++) {
-            String caseRow = i % 2 == 0 ? case1 : case2;
-            String row = board[i].substring(colCount, colCount + 8);
-
-            if (row.equals(caseRow)) {
-                continue;
-            }
-
             for (int j = 0; j < 8; j++) {
-                if (row.charAt(j) != caseRow.charAt(j)) {
+                boolean expected = ((i + j) % 2 == 0) ? firstColor : !firstColor;
+
+                if (arr[x + i][y + j] != expected) {
                     count++;
                 }
             }
         }
-
-        return Math.min(count, 64 - count);
+        count = Math.min(count, 64 - count);
+        return count;
     }
-
 }
