d455efdad4be5b6730f50a0693d29edd9f9aa833
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-01-12 11:47:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 668 ms, Memory: 52820 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,36 +8,35 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        Coordinate[] arr = new Coordinate[N];
+        List<Point> points = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            arr[i] = new Coordinate(x, y);
+            points.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
 
-        Arrays.sort(arr);
+        Collections.sort(points);
 
-        for (int i = 0; i < N; i++) {
-            bw.write(arr[i].toString() + "\n");
+        for (Point point : points) {
+            bw.write(point.toString());
+            bw.newLine();
         }
 
-        bw.flush();
         bw.close();
         br.close();
     }
 
 
-    public static class Coordinate implements Comparable<Coordinate> {
-        int x, y;
+    private static class Point implements Comparable<Point> {
+        private final int x;
+        private final int y;
 
-        public Coordinate(int x, int y) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
 
         @Override
-        public int compareTo(Coordinate o) {
+        public int compareTo(Point o) {
             if (this.x == o.x) {
                 return this.y - o.y;
             }
@@ -55,6 +49,4 @@ public class Main {
             return x + " " + y;
         }
     }
-
-
 }
