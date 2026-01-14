57470887e98c8b7668cddcfa57e99e1a3801e708
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-01-14 11:29:51 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 85732 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,32 +1,20 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
-        int cnt = 0;
-        int number = 666;
-
-        while (true) {
-            if (String.valueOf(number).contains("666")) {
+        int cnt = 1;
+        int num = 666;
+        while (cnt != N) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
                 cnt++;
-                if (cnt == N) {
-                    bw.write(number + "\n");
-                    break;
-                }
             }
-            number++; 
         }
 
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(num);
     }
 }
