80e375f3029f63a2bbb193941a57e471bc0a850f
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-01-14 12:22:06 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 448 ms, Memory: 43392 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -4,32 +4,33 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<Integer, String> num = new HashMap<>();
-        Map<String, Integer> name = new HashMap<>();
+        Map<String, Integer> nameKey = new HashMap<>();
+        String[] numberKey = new String[N + 1];
 
-        for (int i = 1; i < N + 1; i++) {
-            String input = br.readLine();
-            name.put(input, i);
-            num.put(i, input);
+        for (int i = 1; i <= N; i++) {
+            String key = br.readLine();
+            nameKey.put(key, i);
+            numberKey[i] = key;
         }
 
         for (int i = 0; i < M; i++) {
-            String input = br.readLine();
-
-            try {
-                int idx = Integer.parseInt(input);
-                sb.append(num.get(idx)).append("\n");
-            } catch (NumberFormatException e) {
-                sb.append(name.get(input)).append("\n");
+            String question = br.readLine();
+            if (Character.isDigit(question.charAt(0))) {
+                bw.write(numberKey[Integer.parseInt(question)]);
+            } else {
+                bw.write(String.valueOf(nameKey.get(question)));
             }
+            bw.newLine();
         }
 
-        System.out.println(sb);
+        br.close();
+        bw.close();
     }
 }
