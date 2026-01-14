5a9d89d8f25ca85d18e995050b6ed89da748169c
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-01-14 11:37:58 +0900

    [Silver IV] Title: 문자열 집합, Time: 360 ms, Memory: 31224 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Set<String> S = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            S.add(br.readLine());
+        }
+
+        int result = 0;
+        for (int i = 0; i < M; i++) {
+            if (S.contains(br.readLine())) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
