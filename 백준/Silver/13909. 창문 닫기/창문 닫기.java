50f0d5b635966ff28df625f3f7ef4c1658934698
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-01-22 10:18:05 +0900

    [Silver V] Title: 창문 닫기, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -6,12 +6,6 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        int result = 0;
-        for (int i = 1; i * i <= N; i++) {
-            result++;
-        }
-
-        System.out.println(result);
+        System.out.println((int) Math.sqrt(N));
     }
 }
