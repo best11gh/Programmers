08e8b9eafdd0b6f92cef002fa480f418de60bed8
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-01-30 09:27:20 +0900

    [Bronze III] Title: 팩토리얼, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 0){
+            System.out.println(1);
+            return;
+        }
+        
+        int result = 1;
+        for (int i = 1; i <= N; i++) {
+            result *= i;
+        }
+        System.out.println(result);
+    }
+}
