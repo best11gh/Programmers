9a9f83841e345fd919577913064086445b4fc731
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-02-09 22:17:44 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 100 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(fib(N));
+    }
+
+    private static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+}
