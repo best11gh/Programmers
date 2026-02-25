e6a05d7abd3f709c47fc510e7049ccf1e5a2fa2a
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-02-25 12:03:56 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 648 ms, Memory: 118652 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int K;
+    private static int result = -1;
+    private static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println(result);
+    }
+
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int t = 0; t < temp.length; t++) {
+            arr[left + t] = temp[t];
+            count++;
+
+            if (count == K) {
+                result = temp[t];
+                return;
+            }
+        }
+    }
+}
