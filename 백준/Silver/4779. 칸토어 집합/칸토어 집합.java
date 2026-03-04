4b64749796d645edd5701fda637a427aa4f7e6a5
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-03-04 10:51:36 +0900

    [Silver III] Title: 칸토어 집합, Time: 196 ms, Memory: 24440 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -8,21 +8,20 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         while ((num = br.readLine()) != null) {
             int N = Integer.parseInt(num);
-
-            String line = "-".repeat((int) Math.pow(3, N));
-            sb.append(cantor(line)).append("\n");
+            sb.append(cantor(N)).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    private static String cantor(String line) {
-        int size = line.length();
-        if (size == 1) {
+    private static String cantor(int n) {
+        if (n == 0) {
             return "-";
         }
 
-        return cantor(line.substring(0, size / 3)) + " ".repeat(size / 3) + cantor(
-                line.substring(size / 3 * 2));
+        String prev = cantor(n - 1);
+        String space = " ".repeat(prev.length());
+
+        return prev + space + prev;
     }
 }
