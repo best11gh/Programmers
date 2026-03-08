648c7b10f014843ec6392344552db359f5467b84
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-03-08 13:07:41 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N;
+    private static int M;
+
+    private static boolean[] visited;
+    private static int[] arr;
+
+    private static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N];
+        arr = new int[M];
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                if (depth == 0 || arr[depth - 1] < i + 1) {
+                    visited[i] = true;
+                    arr[depth] = i + 1;
+                    dfs(depth + 1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
