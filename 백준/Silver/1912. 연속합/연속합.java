8ba1ff55f67ccf14b4c6c8cf8fce5fb8b4b51a3f
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-03-18 15:56:55 +0900

    [Silver II] Title: 연속합, Time: 236 ms, Memory: 23452 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int current = Integer.parseInt(st.nextToken());
+        int max = current;
+
+        for (int i = 1; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            current = Math.max(current + num, num);
+            max = Math.max(max, current);
+        }
+
+        System.out.println(max);
+    }
+}
