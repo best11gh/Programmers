8ed7b09b9c8ab34384ce7d7f8d8b07fe1595fa10
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-04-02 17:04:12 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 484 ms, Memory: 53608 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,39 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] calSum = new int[N + 1];
-        calSum[0] = 0;
+        int[] prefixSum = new int[N + 1];
+
         st = new StringTokenizer(br.readLine());
-        int tempSum = 0;
-        for (int i = 1; i < N + 1; i++) {
-            tempSum += Integer.parseInt(st.nextToken());
-            calSum[i] = tempSum;
+        for (int i = 1; i <= N; i++) {
+            prefixSum[i] = prefixSum[i - 1] + Integer.parseInt(st.nextToken());
         }
-        
+
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int num1 = Integer.parseInt(st.nextToken());
-            int num2 = Integer.parseInt(st.nextToken());
-
-            int result = calSum[num2] - calSum[num1 - 1];
-            bw.write(result + "\n");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(prefixSum[end] - prefixSum[start - 1]).append("\n");
         }
 
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 }
