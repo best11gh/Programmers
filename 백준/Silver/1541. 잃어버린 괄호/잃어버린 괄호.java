f951c45a6045524c5e1d13d5320535f2a9f24c0a
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-04-10 04:34:33 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,23 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] minusSplit = br.readLine().split("-");
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
 
         int result = 0;
-        for (int i = 0; i < minusSplit.length; i++) {
-            String[] plusSplit = minusSplit[i].split("\\+");
+        boolean isFirst = true;
+
+        while (st.hasMoreTokens()) {
+            String line = st.nextToken();
+            StringTokenizer plus = new StringTokenizer(line, "+");
 
             int sum = 0;
-            for (String num : plusSplit) {
-                sum += Integer.parseInt(num);
+            while (plus.hasMoreTokens()) {
+                sum += Integer.parseInt(plus.nextToken());
             }
 
-            if (i == 0) {
+            if (isFirst) {
                 result += sum;
+                isFirst = false;
             } else {
                 result -= sum;
             }
