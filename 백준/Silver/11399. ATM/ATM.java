343cdfb289192185b2a6a06027bdb80998b9c91e
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-04-10 02:57:32 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,45 +1,25 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         int[] times = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < N; i++) {
             times[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(times);
 
-        int result = 0;
+        int sum = 0;
         for (int i = 0; i < N; i++) {
-            int[] tempTimes = Arrays.copyOf(times, i + 1);
-            int personal = calculateTime(tempTimes, tempTimes.length - 1);
-            result += personal;
+            sum += times[i] * (N - i);
         }
 
-        bw.write(result + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(sum);
     }
-
-    private static int calculateTime(int[] times, int idx) {
-        if (idx == 0) {
-            return times[0];
-        }
-
-        return calculateTime(times, idx - 1) + times[idx];
-    }
-
 }
