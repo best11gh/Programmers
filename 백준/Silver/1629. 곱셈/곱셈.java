a9ff61a60b269383c112a0f7d288ff2e1c03f550
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-04-11 22:20:06 +0900

    [Silver I] Title: 곱셈, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        System.out.println(pow(A, B, C));
+    }
+
+
+    private static long pow(int a, int b, int c) {
+        if (b == 1) {
+            return a % c;
+        }
+
+        long half = pow(a, b / 2, c);
+        if (b % 2 == 0) {
+            return (half * half) % c;
+        } else {
+            return ((half * half) % c) * a % c;
+        }
+    }
+}
