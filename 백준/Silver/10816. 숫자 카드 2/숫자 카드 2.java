3e77882b0fef3e492a60d11d40cb39cc07067862
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-04-17 23:44:23 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1320 ms, Memory: 122420 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -5,14 +5,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Map<Integer, Integer> own = new HashMap<>();
         int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            own.put(num, own.getOrDefault(num, 0) + 1);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr);
 
         StringBuilder sb = new StringBuilder();
 
@@ -20,9 +21,44 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             int num = Integer.parseInt(st.nextToken());
-            sb.append(own.getOrDefault(num, 0)).append(" ");
-        }
 
+            int lowerBound = lowerBound(arr, num);
+            int upperBound = upperBound(arr, num);
+
+            sb.append(upperBound - lowerBound).append(" ");
+        }
         System.out.println(sb);
+    }
+
+    private static int lowerBound(int[] arr, int num) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] >= num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private static int upperBound(int[] arr, int num) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
