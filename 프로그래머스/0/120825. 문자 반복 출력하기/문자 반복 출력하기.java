f5f681568efa396c819bf2a28db48cfbcbe3ee5a
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-05-16 00:02:00 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.03 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (char ch : my_string.toCharArray()) {
+            answer.append(String.valueOf(ch).repeat(n));
+        }
+        
+        
+        return answer.toString();
+    }
+}
