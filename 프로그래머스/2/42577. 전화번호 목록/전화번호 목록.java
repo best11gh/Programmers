151a6028916406e3b5a867a18f2072c91d19c958
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-05-22 11:54:42 +0900

    [level 2] Title: 전화번호 목록, Time: 142.72 ms, Memory: 254 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Set<String> set = new HashSet<>();
+        for (String phone : phone_book) {
+            set.add(phone);
+        }
+        
+        for (String phone : phone_book) {
+            for (int i = 1; i < phone.length(); i++){
+                String str = phone.substring(0, i);
+                
+                if (set.contains(str)) {
+                    return false;
+                }
+            }
+            
+        }
+        return true;
+    }
+}
