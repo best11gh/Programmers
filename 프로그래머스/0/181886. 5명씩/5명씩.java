a595684c5ff869ed078d356e3d395216e8fe9202
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-05-25 20:05:37 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String[] solution(String[] names) {
+        int size = (names.length + 4) / 5;
+        String[] answer = new String[size];
+
+        int idx = 0;
+        for (int i = 0; i < names.length; i++) {
+            if (i % 5 == 0) {
+                answer[idx] = names[i];
+                idx++;
+            }
+        }
+
+        return answer;
+    }
+}
