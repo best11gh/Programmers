5af54be043c7c0281195c4db643b7a95ee28c8c3
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-05-29 14:47:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 20.73 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int num : arr) {
+            if (stack.isEmpty() || stack.peek() != num) {
+                stack.push(num);
+            }
+        }
+        
+        int[] answer = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--){
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}
