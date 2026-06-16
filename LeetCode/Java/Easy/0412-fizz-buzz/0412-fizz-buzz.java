2eb4227ac1aa9ddf62c89c2d22c83f45237d8191
Gahyeon Lee <81560908+best11gh@users.noreply.github.com>
2026-06-16 16:39:32 +0900

    Time: 3 ms (5.2%), Space: 47.2 MB (12.12%) - LeetHub

---
+++ LeetCode/Java/Easy/0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            String result = "";
+
+            if (i % 3 == 0) {
+                result += "Fizz";
+            }
+
+            if (i % 5 == 0) {
+                result += "Buzz";
+            }
+
+            answer.add(result.isEmpty() ? String.valueOf(i) : result);
+        }
+
+        return answer;
+    }
+}
